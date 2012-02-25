a7916a0f81f4715df8890a78cd7993965696ae03
Rudy Gunawan <rudy.gunawan@gmail.com>
2012-02-26 02:34:31 +0800

    test

---
+++ Lapor-Droid/src/com/teamuphome/lapor/bean/Location.java
@@ -0,0 +1,18 @@
+package com.teamuphome.lapor.bean;
+
+public class Location {
+	private double longitude;
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	private double latitude;
+}
